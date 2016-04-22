306cf5fe240ddfe5b9c8d20b78f84ee5312bf18c
mmagnuski <mmagnuski@swps.edu.pl>
2016-04-22 21:57:28 +0200

    new module for viz - currently uses set_axes_equal (for 3D matplotlib plots)

---
+++ mypy/viz.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# TODOs:
+# - [ ] adapt set_axes_equal to use map()
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+
+    copied from:
+    http://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_mean = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_mean = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_mean = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_mean - plot_radius, x_mean + plot_radius])
+    ax.set_ylim3d([y_mean - plot_radius, y_mean + plot_radius])
+    ax.set_zlim3d([z_mean - plot_radius, z_mean + plot_radius])
