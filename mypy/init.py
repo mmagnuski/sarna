8b349285b810e096e87818f57e4d4a90245ce5a9
Mikolaj Magnuski <mmagnuski@swps.edu.pl>
2016-05-01 17:28:19 +0200

    Update init.py

--- mypy/init.py
+++ mypy/init.py
@@ -3,11 +3,16 @@
 
 import os
 import os.path as op
-from os.path import join as pj
+from os.path import join
 from glob import glob
 
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+try:
+  from showit import image
+except ImportError:
+  pass
 # import seaborn as sns
