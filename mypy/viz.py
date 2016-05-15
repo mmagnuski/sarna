bf3e8a3bce56cbc225ac78e2755d64f64bc31555
Mikolaj Magnuski <mmagnuski@swps.edu.pl>
2016-05-15 22:07:16 +0200

    modify set_axes_equal (now set_3d_axes_equal)

--- mypy/viz.py
+++ mypy/viz.py
@@ -1,7 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # TODOs:
-# - [ ] adapt set_axes_equal to use map()
 # MultiDimView:
 # - [ ] add colorbar
 # - [ ] add topo view
@@ -94,7 +94,7 @@ class MultiDimView(object):
             self.refresh()
 
 
-def set_axes_equal(ax):
+def set_3d_axes_equal(ax):
     '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
     cubes as cubes, etc..  This is one possible solution to Matplotlib's
     ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
@@ -102,24 +102,21 @@ def set_axes_equal(ax):
     Input
       ax: a matplotlib axis, e.g., as output from plt.gca().
 
-    copied from:
+    modified from:
     http://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
     '''
 
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
+    x_lim, y_lim, z_lim = ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()
 
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_mean = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_mean = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_mean = np.mean(z_limits)
+    def get_range(lim):
+        return lim[1] - lim[0], np.mean(lim)
+    x_range, x_mean = get_range(x_lim)
+    y_range, y_mean = get_range(y_lim)
+    z_range, z_mean = get_range(z_lim)
 
     # The plot bounding box is a sphere in the sense of the infinity
     # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
 
     ax.set_xlim3d([x_mean - plot_radius, x_mean + plot_radius])
     ax.set_ylim3d([y_mean - plot_radius, y_mean + plot_radius])
