296b6f774c31708d30e1b63fabf762a30678a202
mmagnuski <mmagnuski@swps.edu.pl>
2016-07-29 14:57:25 +0200

    fix my_cwt

--- mypy/freq.py
+++ mypy/freq.py
@@ -62,11 +62,20 @@ def plot_topo_and_psd(inst, mean_psd, freqs, channels):
     ax[1].plot(freqs[plot_freq], chan_avg[plot_freq], color='k', lw=2)
 
 
-def _my_cwt(epochs, Ws, times=None, picks=None):
+# - [ ] average param
+# - [ ] use_fft?
+def _my_cwt(inst, Ws, times=None, picks=None, fast_dot=True):
     """Compute cwt with dot products at specific times.
 
     Parameters
     ----------
+    inst - Epochs | Raw
+        Object containing data to process.
+    Ws - list of numpy arrays
+        List of wavelets to use in time-frequency decomposition.
+    times - array-like
+        Time points in seconds defining centers of consecutive time windows.
+    picks - channels to use in time-frequency decomposition.
 
     Returns
     -------
@@ -74,12 +83,18 @@ def _my_cwt(epochs, Ws, times=None, picks=None):
         single-trial time-frequency decomposition of `epochs`. Numpy array of
         size (n_epochs, n_channels, n_wavelets, n_times_of_interest)
     """
-    X = epochs.get_data()
-    n_epochs, n_channels, n_times = X.shape
+    from mne.io import _BaseRaw
+    from mne.utils import _get_fast_dot
+
+    is_raw = isinstance(inst, _BaseRaw)
+    X = inst._data if is_raw else inst.get_data()
+    if is_raw:
+        n_channels, n_times = X.shape
+    else:
+        n_epochs, n_channels, n_times = X.shape
 
     # check wavelets
     W_sizes = [W.size for W in Ws]
-    # Ws_max_size = max(W_sizes)
 
     if picks is None:
         picks = list(range(n_channels))
@@ -87,23 +102,46 @@ def _my_cwt(epochs, Ws, times=None, picks=None):
         # some auto settings...
         pass
 
-    times_ind = find_index(epochs.times, times)
+    times_ind = inst.time_as_index(times)
     n_times_out = len(times)
     n_freqs = len(Ws)
 
-    # ADD check which time indices can be used for which wavelets...
+    # each wavelet has its own timesteps (in case things don't fit)
+    w_time_lims = list()
+    for ws in W_sizes:
+        hlf = ws / 2
+        l, h = map(int, (np.ceil(hlf), np.floor(hlf)))
+        good_times = (times_ind >= l) & (times_ind <= (n_times - h))
+        w_time_lims.append(np.where(good_times)[0][[0, -1]])
+    w_time_lims = np.vstack(w_time_lims)
 
     # reshape data, prepare output
-    data = data.reshape((n_epochs * n_channels, n_times))
-    tfr = np.zeros((n_freqs, n_epochs * n_channels, n_times_out),
-                   dtype=np.complex128)
+    if not is_raw:
+        X = X.reshape((n_epochs * n_channels, n_times))
+        tfr = np.empty((n_freqs, n_epochs * n_channels, n_times_out),
+                       dtype=np.complex128)
+    else:
+        tfr = np.empty((n_freqs, n_channels, n_times_out),
+                       dtype=np.complex128)
+    tfr.fill(np.nan)
 
-    # Loop across wavelets
+    # get dot operator:
+    if fast_dot:
+        dot = _get_fast_dot()
+    else:
+        dot = np.dot
+
+    # Loop across wavelets, compute power
     for ii, W in enumerate(Ws):
-        l, r = W_sizes[ii] / 2. * np.array([-1, 1])
-        l, r = np.floor(l), np.floor(r)
+        l, r = map(int, np.floor(W_sizes[ii] / 2. * np.array([-1, 1])))   
+        t_start, t_end = w_time_lims[ii, :] + [0, 1]
         # loop across time windows
-        for ti, tind in enumerate(times_ind):
-            tfr[ii, :, t] = np.dot(data[:, tind - l:tind + r], W)
-    return np.transpose(tfr.reshape((n_freqs, n_epochs, n_channels, n_times)),
-                        (1, 2, 0, 3))
+        for ti, tind in enumerate(times_ind[t_start:t_end]):
+            tfr[ii, :, ti + t_start] = dot(X[:, tind + l:tind + r], W)
+
+    if is_raw:
+        tfr = np.transpose(tfr, (1, 0, 2))
+    else:
+        tfr = np.transpose(tfr.reshape((n_freqs, n_epochs, n_channels,
+            n_times_out)), (1, 2, 0, 3))
+    return tfr
