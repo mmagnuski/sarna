01d013ad110dcdcee78decc71994039770244cd2
mmagnuski <mmagnuski@swps.edu.pl>
2016-08-18 15:41:07 +0200

    add read_set_events

--- mypy/events.py
+++ mypy/events.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from scipy.io import loadmat
+
 
 # TODOs:
 # - [ ] correct_egi_channel_names could live in a separate module
@@ -158,14 +160,101 @@ def correct_egi_channel_names(eeg):
 	eeg.rename_channels(corr_ch_names)
 
 
+def reject_events_in_bad_segments(events, bad_segments, around_event=(-10,10), remove_types=None):
+	'''removes events that coincide with bad segments
+
+	parameters
+	----------
+	events 		 - mne events array
+	bad_segments - N by 2 matrix with bad segment info
+				   each row is a bad segment, first column is the
+				   bad segment onset and the second column is the
+				   bad segment offset (in samples)
+	around_event - (lowerlim, higherlim) box limits around
+				   each event. If a bad segment overlaps
+				   with the box, the event is rejected.
+				   Box limits are in samples.
+				   (-500, 1000) means 500 samples before
+				   up to 1000 samples after, but one can
+				   create box that does not contain the
+				   event like (250, 500)
+	remove_types - list or numpy array of event types (int)
+				   that should be checked for removal
+
+	returns
+	-------
+	events - corrected events array
+	'''
+	if remove_types is None:
+		test_events = np.arange(events.shape[0])
+	else:
+		test_events = np.vstack([events[:,2] == x for x in remove_types])
+		test_events = np.where(np.any(test_events, axis=0))
+
+	ev = events[test_events, 0]
+	ev = np.vstack([ev + x for x in around_event]).T
+	remove = np.zeros(ev.shape[0], dtype='bool')
+	for ii in range(ev.shape[0]):
+		if np.any(np.logical_and(ev[ii,0] >= bad_segments[:,0],
+			ev[ii,0] < bad_segments[:,1])):
+			remove[ii] = True
+			continue
+		if np.any(np.logical_and(ev[ii,1] > bad_segments[:,0],
+			ev[ii,1] <= bad_segments[:,1])):
+			remove[ii] = True
+			continue
+		if np.any(np.logical_and(bad_segments[:,0] >= ev[ii,0],
+			bad_segments[:,0] < ev[ii,1])):
+			remove[ii] = True
+	# remove events
+	remove_ind = test_events[remove]
+	if remove_ind.shape[0] > 0:
+		events = np.delete(events, remove_ind, axis=0)
+	return events
+
+
 def create_middle_events(events, min_time=14., sfreq=250):
-    '''Put raven events in the trial centre,
-    deleting events that begin trials shorter than
-    min_time.'''
-    ep_start = np.logical_or(events[:,2] == 1, events[:,2] == 0)
-    ep_end = np.logical_or(events[:,2] == 10, events[:,2] == 11)
-    ep_len = events[ep_end, 0] - events[ep_start, 0]
-    long_enough = ep_len > int(min_time * sfreq)
-    new_events = events[ep_start,:]
-    new_events[:,0] += np.round(ep_len / 2).astype('int')
-    return new_events[long_enough, :]
+	'''Put raven events in the trial centre,
+	deleting events that begin trials shorter than
+	min_time.'''
+	ep_start = np.logical_or(events[:,2] == 1, events[:,2] == 0)
+	ep_end = np.logical_or(events[:,2] == 10, events[:,2] == 11)
+	ep_len = events[ep_end, 0] - events[ep_start, 0]
+	long_enough = ep_len > int(min_time * sfreq)
+	new_events = events[ep_start,:]
+	new_events[:,0] += np.round(ep_len / 2).astype('int')
+	return new_events[long_enough, :]
+
+
+# TODO:
+# - [ ] currently reads set events, should check epoch field...
+def read_set_events(filename, ignore_fields=None):
+	'''Open set file, read epoch events and turn them into a dataframe
+
+	Parameters
+	----------
+	filename: str
+		Name of the set file to read (can be absolute or relative path)
+	ignore_fields: list of str | None
+		Epoch event fields to ignore
+
+	Returns
+	-------
+	df: pandas.DatFrame
+		Events read into dataframe
+	'''
+	EEG = loadmat(filename, uint16_codec='latin1',
+				  struct_as_record=False, squeeze_me=True)['EEG']
+	flds = [f for f in dir(EEG.event[0]) if not f.startswith('_')]
+	events = EEG.event
+	df_dict = dict()
+	for f in flds:
+		df = df_dict[f] = [ev.__getattribute__(f) for ev in events]
+	df = pd.DataFrame(df_dict)
+
+	# reorder columns:
+	take_fields = ['epoch', 'type']
+	ignore_fields = list() if ignore_fields is None else ignore_fields
+	take_fields.extend([col for col in df.columns if not
+					 (col in take_fields or col in ignore_fields)])
+	return df.loc[:, take_fields]
