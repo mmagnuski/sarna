3f09d64988ff6bfc5b210ccb0b44eeb647fcbd22
mmagnuski <mmagnuski@swps.edu.pl>
2016-09-05 16:20:59 +0200

    small changes to init.py

--- mypy/init.py
+++ mypy/init.py
@@ -3,13 +3,19 @@
 
 import os
 import os.path as op
-from os.path import join
+from os.path import join as pjoin
 from glob import glob
 
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+from scipy.io import loadmat, savemat
+from scipy import stats
+# from scipy.stats import distributions as dist
+# from scipy import signal
+# from scipy import fftpack
 
 try:
   from showit import image
@@ -20,6 +26,6 @@ except ImportError:
 from mypy import whos, find_index
 from mypy.freq import dB
 
-def pwd():
-    return os.getcwd()
-cwd = pwd
+# def pwd():
+#     return os.getcwd()
+# cwd = pwd
