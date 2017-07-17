9fea42f15dd5fd101c95f4a76c0aa636330923b5
mmagnuski <mmagnuski@swps.edu.pl>
2017-07-17 21:24:36 +0200

    FIX: fix read_rej

--- mypy/events.py
+++ mypy/events.py
@@ -236,12 +236,13 @@ def read_set_events(filename, ignore_fields=None):
 
 
 def read_rej(fname, sfreq, bad_types=['reject']):
+	from mne import Annotations
 	rej = pd.read_table(fname)
 	rej.loc[:, ['start', 'end']] = rej.loc[:, ['start', 'end']] / sfreq
 	onset, duration = rej.start.values, (rej.end - rej.start).values
-    description = ['BAD ' + val if val in bad_types else val
+	description = ['BAD ' + val if val in bad_types else val
 				   for val in rej.type.tolist()]
-    return mne.Annotations(onset, duration, description)
+	return Annotations(onset, duration, description)
 
 
 # - [ ] develop this function a little better
