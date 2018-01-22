3c49c92c0db772c8dd3011d8010524af71c35e58
mmagnuski <mmagnuski@swps.edu.pl>
2018-01-22 11:45:51 +0100

    ENH: init file for stats

---
+++ mypy/init_stats.py
@@ -0,0 +1,9 @@
+from scipy.stats import (ttest_rel, ttest_ind, levene, zscore, pearsonr,
+                         spearmanr, kendalltau, f_oneway)
+
+# statsmodels api
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+# distributions
+from scipy.stats import distributions
