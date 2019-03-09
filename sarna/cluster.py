aac0be4aec80a61167a3192e04af96264e82bc01
mmagnuski <mmagnuski@swps.edu.pl>
2019-03-09 16:39:04 +0100

    DEV: 3d clustering was moved to borsar

--- sarna/cluster.py
+++ sarna/cluster.py
@@ -190,18 +190,9 @@ def plot_neighbours(inst, adj_matrix, color='gray', kind='3d'):
     return fig
 
 
-def get_cluster_fun(data, adjacency=None):
-    has_adjacency = adjacency is not None
-    if data.ndim == 3 and has_adjacency:
-        if has_numba():
-            from .cluster_numba import cluster_3d_numba
-            return cluster_3d_numba
-        else:
-            return cluster_3d
-
-
 def cluster(data, adjacency=None):
-    clst_fun = get_cluster_fun(data, adjacency)
+    from borsar.cluster import _get_cluster_fun
+    clst_fun = _get_cluster_fun(data, adjacency)
     return clst_fun(data, adjacency)
 
 
@@ -211,67 +202,6 @@ def cluster_1d(data, connectivity=None):
     if connectivity is not None:
         connectivity = sparse.coo_matrix(connectivity)
     return _find_clusters(data, 0.5, connectivity=connectivity)
-
-
-# - [ ] check timing on some 3d matrix (3d random smoothed?)
-# - [ ] compare to some other algo
-def cluster_3d(matrix, chan_conn):
-    '''
-    Parameters
-    ----------
-    matrix : numpy array
-        Matrix of shape ``(channels, dim2, dim3)``
-    chan_conn : numpy array
-        2d boolean matrix with information about channel adjacency.
-        If ``chann_conn[i, j]`` is True that means channel i and j are
-        adjacent.
-
-    Returns
-    -------
-    clusters - 3d integer matrix with cluster labels
-    '''
-    # matrix has to be bool
-    assert matrix.dtype == np.bool
-
-    # nested import
-    from skimage.measure import label
-
-    # label each channel separately
-    clusters = np.zeros(matrix.shape, dtype='int')
-    max_cluster_id = 0
-    n_chan = matrix.shape[0]
-    for ch in range(n_chan):
-        clusters[ch, :, :] = label(matrix[ch, :, :],
-            connectivity=1, background=False)
-
-        # relabel so that layers do not have same cluster ids
-        num_clusters = clusters[ch, :, :].max()
-        clusters[ch, clusters[ch,:] > 0] += max_cluster_id
-        max_cluster_id += num_clusters
-
-    # unrolled views into clusters for ease of channel comparison:
-    unrolled = [clusters[ch, :].ravel() for ch in range(n_chan)]
-    # check channel neighbours and merge clusters across channels
-    for ch in range(n_chan - 1): # last chan will be already checked
-        ch1 = unrolled[ch]
-        ch1_ind = np.where(ch1)[0]
-        if ch1_ind.shape[0] == 0:
-            continue # no clusters, no fun...
-
-        # get unchecked neighbours
-        neighbours = np.where(chan_conn[ch + 1:, ch])[0]
-        if neighbours.shape[0] > 0:
-            neighbours += ch + 1
-
-            for ngb in neighbours:
-                ch2 = unrolled[ngb]
-                for ind in ch1_ind:
-                    # relabel clusters if adjacent and not the same id
-                    if ch2[ind] and not (ch1[ind] == ch2[ind]):
-                        c1 = min(ch1[ind], ch2[ind])
-                        c2 = max(ch1[ind], ch2[ind])
-                        clusters[clusters == c2] = c1
-    return clusters
 
 
 def cluster_spread(cluster, connectivity):
