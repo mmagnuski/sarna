7966edd1892d1fb9feea91a18f97f557c0bb1552
1 <kid.rub@gmail.com>
2020-07-17 12:52:47 +0300

    new functions

--- sarna/freq.py
+++ sarna/freq.py
@@ -14,6 +14,9 @@
 
 import numpy as np
 # from numba import jit
+import mne
+from warnings import warn
+from sarna.utils import group
 
 
 def dB(x):
@@ -226,3 +229,143 @@ def _cwt_loop(X, times_ind, Ws, W_sizes, w_time_lims):
         for ti, tind in enumerate(times_ind[t_start:t_end]):
             tfr[ii, :, ti + t_start] = np.dot(X[:, tind + l:tind + r], W)
     return tfr
+
+
+def _find_high_amplitude_periods(data, amp_z_thresh=2.5, min_period=0.1,
+                                 extend=None):
+    '''
+    Find segments of high amplitude in filtered, hilbert-transformed signal.
+
+    Parameters
+    ----------
+    data : mne.Epochs
+        Epoched data. Must be filtered and hilbert-transformed.
+    amp_z_thresh : float
+        Z score threshold defining high amplitude periods. Defaults to ``2.5``.
+    min_period : float
+        Minimum length of high amplitude period in seconds.
+        Defaults to ``0.1``.
+    extend : float | None
+        Extend each period by this many seconds. Defaults to ``None`` which
+        does not extend the periods.
+
+    Returns
+    -------
+    periods : np.ndarray
+        Numpy array of (n_periods, 3) shape. The columns are: epoch index,
+        within-epoch sample index of period start, within-epoch sample index of
+        period end.
+    '''
+
+    # amplitude periods
+    n_epochs, n_channels, n_samples = data._data.shape
+    comp_data = data._data.transpose([1, 0, 2]).reshape((n_channels, -1))
+
+    # find segments with elevated amplitude
+    envelope = np.abs(comp_data).mean(axis=0)
+    envelope_z = envelope / envelope.mean(axis=0, keepdims=True)
+    grp = group(envelope_z > amp_z_thresh)
+    # check if there are some segments that start at one epoch
+    # and end in another
+    # -> if so, they could be split, but we will ignore them for now
+    epoch_idx = np.floor(grp / n_samples)
+    epoch_diff = np.diff(epoch_idx, axis=1)
+    epochs_joint = epoch_diff > 0
+    if epochs_joint.any():
+        msg = ('{:d} high-amplitude segments will be ignored because'
+               ' the developer was lazy.')
+        warn(msg.format(epochs_joint.sum()))
+        epoch_diff = epoch_diff[~epochs_joint, :]
+
+    segment_len = np.diff(grp, axis=1)
+    good_length = segment_len[:, 0] * (1 / data.info['sfreq']) > min_period
+    grp = grp[good_length, :]
+    epoch_idx = np.floor(grp[:, [0]] / n_samples).astype('int')
+    grp -= epoch_idx * n_samples
+
+    if extend is not None:
+        extend_samples = int(np.round(extend * data.info['sfreq']))
+        extend_samples = np.array([-extend_samples, extend_samples])
+        grp += extend_samples[np.newaxis, :]
+
+        # check for limits
+        msk1 = grp[:, 0] < 0
+        msk2 = grp[:, 1] >= n_samples
+        grp[msk1, 0] = 0
+        grp[msk2, 1] = n_samples - 1
+
+    return np.append(epoch_idx, grp, axis=1)
+
+
+def _create_low_amp_annotations(raw, events, event_id, freq, tmin, sel_chans,
+                                amp_z_thresh=2.):
+    '''
+    Creates annotations for fragments with low amplitude.
+    Parameters
+    ----------
+    raw: mne.Raw
+          Raw file to use.
+    events: numpy array | None
+        Mne events array of shape (n_events, 3). If None (default) `tmin` and
+        `tmax` are not calculated with respect to events but the whole time
+        range of the `raw` file.
+    event_id: list | numpy array
+        Event types (IDs) to use in defining segments for which psd is
+        computed. If None (default) and events were passed all event types are
+        used.
+    tmin: float
+        Lower edge of each segment in seconds. If events are given the lower
+        edge is with respect to each event. If events are not given only one
+        segment is used and `tmin` denotes the lower edge of the whole `raw`
+        file.
+    freq :
+        Frequencies for which low amplitude periods will be calculated.
+    sel_chans : list
+        List of channels for which low amplitude periods will be calculated.
+    amp_z_thresh : float
+        Z score threshold defining high amplitude periods. Defaults to ``2.``
+
+    Returns
+    -------
+    low_amp_annot : mne.Annotations
+                    Annotations with low amplitude segment
+
+    '''
+    filt_raw = raw.copy().filter(freq[0], freq[1])
+    epochs = mne.Epochs(filt_raw, events, event_id=event_id, tmin=tmin,
+                        tmax=60.0, baseline=None, preload=True,
+                        reject_by_annotation=False)
+
+    filt_hilb_data = epochs.copy().pick(sel_chans).apply_hilbert()
+    # find fragments with high amplitude
+    hi_amp = _find_high_amplitude_periods(filt_hilb_data,
+                                          amp_z_thresh=amp_z_thresh)
+    # convert those fragments to annotations with low amplitude
+    amp_neg_annot = np.zeros((hi_amp.shape[0] + 1, 2))
+    sfreq = raw.info['sfreq']
+    events_sel = events[:, 2] == event_id
+    epoch_events = events[events_sel, 0]
+    tmin_samples = int(np.round(tmin * sfreq))
+    epoch_idx, start, _ = hi_amp[0, :]
+
+    amp_neg_annot[0, :] = [0, start + epoch_events[epoch_idx] + tmin_samples]
+
+    for idx in range(hi_amp.shape[0] - 1):
+        epoch_idx1, _, end = hi_amp[idx, :]
+        epoch_idx2, start, _ = hi_amp[idx + 1, :]
+        amp_neg_annot[idx + 1, 0] = (end + epoch_events[epoch_idx1] +
+                                     tmin_samples)
+        amp_neg_annot[idx + 1, 1] = (start + epoch_events[epoch_idx2] +
+                                     tmin_samples - amp_neg_annot[idx + 1, 0])
+
+    n_samples = raw._data.shape[1]
+    epoch_idx, _, end = hi_amp[-1, :]
+    raw_start_samples = end + epoch_events[epoch_idx] + tmin_samples
+    amp_neg_annot[-1, :] = [raw_start_samples, n_samples - raw_start_samples]
+    amp_neg_annot_sec = amp_neg_annot / sfreq
+
+    n_segments = amp_neg_annot.shape[0]
+    low_amp_annot = mne.Annotations(amp_neg_annot_sec[:, 0],
+                                    amp_neg_annot_sec[:, 1],
+                                    ['BAD_lowamp'] * n_segments)
+    return low_amp_annot
