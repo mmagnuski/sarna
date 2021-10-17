9cc3f4e2bc73800c7cf4040733ce67ec567e7c30
mmagnuski <mmagnuski@swps.edu.pl>
2021-10-17 21:35:24 +0200

    ENH: new functions for GED

--- sarna/ged.py
+++ sarna/ged.py
@@ -6,9 +6,8 @@ from borsar.viz import Topo
 
 
 # TODO: add plot_eig for plotting eigenvalues
-# TODO: add save and read_ged?
 # TODO: we use eig.real - maybe a warning should be added if we get complex
-#       values
+#       values?
 class GED(object):
     def __init__(self, cov_S=None, cov_R=None, reg=None, eig=None,
                  filters=None, patterns=None, description=None):
@@ -25,6 +24,11 @@ class GED(object):
         reg : None | float
             Regularization factor, from 0 - 1. If ``None`` then regularization
             is not performed.
+
+        Notes
+        -----
+        The ``.filters`` attribute contains n_channels x n_filters array.
+        The ``.patterns`` attribute contains n_patterns x n_channels array.
         '''
         # CONSIDER - could also be first constructed and the ``.fit``
         #            to be complementary with sklearn...
@@ -169,3 +173,126 @@ def _deal_with_idx(idx):
         else:
             idx = [idx]
     return idx
+
+
+# TODO: this could be changed to work on annotations too (I likely meant: to
+#       compute covariance only for time periods contained in given annotation)
+# TODO: default arguments should be changed
+def compute_cov_raw(raw, events, event_id=11, tmin=1., tmax=60.):
+    '''Compute covariance on long segments of raw data marked by events.
+    '''
+    # epoch data and turn bad annotations to NaNs
+    raw = raw.copy()
+    raw._data = raw.get_data(reject_by_annotation='NaN', verbose=False)
+    epochs = mne.Epochs(raw, events, event_id=event_id, tmin=tmin, tmax=tmax,
+                        baseline=None, preload=True, verbose=False,
+                        reject_by_annotation=False)
+
+    # roll out the data to channels x times
+    n_epochs, n_channels, n_samples = epochs._data.shape
+    data = epochs._data.transpose((1, 0, 2)).reshape(
+        n_channels, n_epochs * n_samples)
+
+    # ignore NaN's and compute covariance
+    not_nan = ~np.isnan(data).any(axis=0)
+    data = data[:, not_nan]
+    cov = (data @ data.T) / (not_nan.sum() * n_epochs)
+    return cov
+
+
+# TODO: make it work also for Epochs
+# TODO: allow to pass arguments to compute_cov
+def compute_narrowband_ged(raw, events, freq, cov_raw=None, freq_around=None,
+                           filter_args=None, reg=0.05):
+    # default values
+    freq_around = np.array([-0.5, 0.5]) if freq_around is None else freq_around
+    filter_args = dict(h_trans_bandwidth=0.5, l_trans_bandwidth=0.5,
+                       verbose=False) if filter_args is None else filter_args
+    if isinstance (freq_around, Real):
+        freq_around = np.array([freq_around] * 2)
+
+    if cov_raw is None:
+        cov_raw = compute_cov_raw(raw, events)
+
+    f1, f2 = freq + freq_around
+    raw_theta = raw.copy().filter(f1, f2, **filter_args)
+    cov_theta = compute_cov_raw(raw_theta, events)
+    ged = GED(cov_theta, cov_raw, reg=reg)
+    return ged
+
+
+# TODO: could use more than one component per frequency and then return
+#       an array of components x channels x frequencies (for example)
+def ged_scan_freqs(raw, freq_centers, freq_around=None, filter_args=None,
+                   reg=0.05):
+    '''Compute narrowband GED for a range of frequencies.
+
+    Only the first component per frequency is saved (or rather - its filter and
+    pattern).
+
+    Parameters
+    ----------
+    raw : mne.io.Raw
+        Instance of raw data to use.
+    freq_centers : numpy.ndarray
+        Frequency centers to use during filtering. GED will be computed for
+        each frequency.
+    freq_around : float | numpy.ndarray
+        Width of the filter. Either one value specifying full width or
+        two-element array specifying distance from frequency center on the left
+        and right.
+    filter_args : dict
+        Dictionary with additional arguments used during filtering.
+    reg : float
+        Regularization value used in GED computation. Defaults to ``0.5``.
+
+    Returns
+    -------
+    eigs : numpy.ndarray
+        Array of component eigenvalues - one per frequency tested. Only the
+        first component's eigenvalue is saved for each frequency.
+    maps : numpy.ndarray
+        Channels by frequencies array of component patterns. Only the
+        first component's pattern is saved for each frequency.
+    vecs : numpy.ndarray
+        Channels by frequencies array of component eigenvectors. Only the
+        first component's eigenvector is saved for each frequency.
+    '''
+    from numbers import Real
+    from tqdm import tqdm
+    cov_raw = compute_cov_raw(raw, events)
+
+    eigs, maps, vecs = list(), list(), list()
+    for freq in tqdm(freq_centers):
+        ged = compute_narrowband_ged(raw, events, freq, cov_raw=cov_raw,
+                                     freq_around=freq_around,
+                                     filter_args=filter_args, reg=reg)
+        eigs.append(ged.eig[0])
+        maps.append(ged.patterns[0, :])
+        vecs.append(ged.filters[:, 0])
+
+    maps = np.stack(maps, axis=1)
+    vecs = np.stack(vecs, axis=1)
+    eigs = np.array(eigs)
+
+    return eigs, maps, vecs
+
+
+# TODO: allow to show other number of components than 6
+# TODO: in eigenvalues plot mark also which components are shown
+def plot_ged_comps(ged, info, psd=None, fmin=1., fmax=15.):
+    fig = plt.figure(figsize=(15, 5))
+    gs = plt.GridSpec(2, 5)
+
+    psd_ax = fig.add_subplot(gs[:2, :2])
+    topo_ax = list()
+    for row in range(2):
+        topo_ax += [fig.add_subplot(gs[row, idx]) for idx in range(2, 5)]
+    ged.plot(info, axes=topo_ax)
+
+    if psd is not None:
+        psd = psd.crop(fmin=1, fmax=15)
+        psd_ax.plot(psd.freqs, psd.data.T)
+        psd_ax.legend(np.arange(1, 7))
+    else:
+        psd_ax.plot(ged.eig)
