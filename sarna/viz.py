c49dd2798f82b488ca92841a370fd5d797d1c436
mmagnuski <mmagnuski@swps.edu.pl>
2024-01-17 12:48:45 +0100

    FIX: fix the fix for glassplot

--- sarna/viz.py
+++ sarna/viz.py
@@ -520,7 +520,7 @@ def glassplot(x=None, y=None, data=None, x_width=0.2, zorder=4,
         if len(swarms) > 0:
             good_groups = len(swarms) == len(categories)
             if not good_groups:
-                swarms[:len(categories)]
+                swarms = swarms[:len(categories)]
             colors = [swarm.get_facecolor()[0] for swarm in swarms]
             colors = [color[:3] for color in colors]  # ignore alpha
 
